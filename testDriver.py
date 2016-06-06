38ce71f907b13605d24278ba586a7671dec7e5db
Anna Sokolova <anna.a.kozlenko@gmail.com>
2016-06-06 14:11:05 -0600

    poly to graph mostly working

---
+++ testDriver.py
@@ -0,0 +1,18 @@
+import polygonsToGraph
+from Graph import Graph, Point, Node
+from AStar import AStar
+import visualizationTool
+
+points = [Point(600, 500), Point(600, 510), Point(610, 500), Point(610, 510)]
+polygons = [polygonsToGraph.Polygon(points)]
+start = Point(51,55)
+graph = polygonsToGraph.translateToGraph(start, polygons)
+visualizationTool.visualizeGraph(graph, polygons)
+astar = AStar(graph)
+path = astar.run()
+visualizationTool.visualizeResultFromNodes(graph, path, polygons)
+graph.saveToFile("test.txt")
+newgraph = Graph()
+newgraph.importFromFile("test.txt")
+newgraph.saveToFile("test2.txt")
+#TODO: visualize
